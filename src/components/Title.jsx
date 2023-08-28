42204e7fb763703f7499de4139cdf008015a7aa7
punyeeet <puneetpradhan007@gmail.com>
2023-08-29 00:50:49 +0530

    project commit

---
+++ src/components/Title.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Title.css'
+
+const Title = () => {
+  return (
+    <div className='title'>
+    <h1>
+  <span>Con_Hub</span>
+  <div className="message">
+    <div className="word1">create</div>
+    <div className="word2">code</div>
+    <div className="word3">network</div>
+  </div>
+</h1>
+<hr></hr>
+    </div>
+  )
+}
+
+export default Title
